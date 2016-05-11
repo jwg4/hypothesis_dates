26b34862e30d3bc62317b785d9545e5f8114c35f
Jack <jack.grahl@yahoo.co.uk>
2016-05-11 01:15:43 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='hypothesis_dates',
+      version='0.1.0',
+      description='Hypothesis add-on for testing code involving dates',
+      url='http://github.com/jwg4/hypothesis_dates',
+      author='Jack Grahl',
+      author_email='jack.grahl@yahoo.co.uk',
+      license='GNU GPL v3',
+      packages=['hypothesis_dates'],
+      zip_safe=False)
+
