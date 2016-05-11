1faaee2c5702eb46119359bc6818d5072e718086
Jack <jack.grahl@yahoo.co.uk>
2016-05-11 01:49:09 +0100

    A working DateStrategy, which only generates one date.

--- hypothesis_dates/__init__.py
+++ hypothesis_dates/__init__.py
@@ -1,2 +1,12 @@
+import datetime
+
+from hypothesis.strategies import defines_strategy
+from hypothesis.searchstrategy.strategies import SearchStrategy
+
+class DateStrategy(SearchStrategy):
+    def do_draw(self, data):
+        return datetime.date(2010, 8, 1)
+
+@defines_strategy
 def dates():
-    pass
+    return DateStrategy()
