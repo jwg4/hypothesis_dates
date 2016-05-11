863af27312a0162638e6f4778607b30bc434d421
Jack <jack.grahl@yahoo.co.uk>
2016-05-11 01:31:08 +0100

    Correctly put the test in a test class.

--- examples/test_example.py
+++ examples/test_example.py
@@ -2,6 +2,7 @@ from hypothesis import given
 from hypothesis_dates import dates
 
 import datetime
+import unittest
 
 def iso_to_gregorian(iso_year, iso_week, iso_day):
     "Gregorian calendar date for the given ISO year, week and day"
@@ -9,6 +10,7 @@ def iso_to_gregorian(iso_year, iso_week, iso_day):
     _, fifth_jan_week, fifth_jan_day = fifth_jan.isocalendar()
     return fifth_jan + datetime.timedelta(days=iso_day-fifth_jan_day, weeks=iso_week-fifth_jan_week)
 
-@given(dates())
-def test_date_conversion(d):
-    assert iso_to_gregorian(datetime.isocalendar(d)) == d
+class TestDateConversion(unittest.TestCase):
+    @given(dates())
+    def test_date_conversion(d):
+        assert iso_to_gregorian(datetime.isocalendar(d)) == d
